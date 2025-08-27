d75d23c973091014d22603a3d0a6e3fb4b7167ff
fareedfmd <67195635+fareedfmd@users.noreply.github.com>
2025-08-27 20:02:06 +0530

    Add files via upload

---
+++ Assignment1_Module2_Task1.py
@@ -0,0 +1,25 @@
+# Task 1: Perform Basic Mathematical Operations
+
+# take two numbers from the user
+
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+
+# To get a new line gap
+print('\n')
+
+# Addition operation
+add = a + b
+print("Addition:", add)
+
+#Subtraction operation
+sub = a - b
+print("Subtraction:", sub)
+
+# Multiplication operation
+mul = a * b
+print("Multiplication:", mul)
+
+# Division operation
+div = a / b
+print("Division:", div)
---
+++ Assignment1_Module2_Task2.py
@@ -0,0 +1,11 @@
+# Task 2: Create a Personalized Greeting
+
+# takes user's first name & last name as input
+
+fname = input("Enter your first name:")
+lname = input("Enter your last name:")
+fullname = fname + ' ' + lname
+
+print("Hello,",fullname,"! Welcome to the Python program.")
+
+#print(f'Hello, {fullname}! Welcome to the Python program.')
